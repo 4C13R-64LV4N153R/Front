93330d59b7a11b7d0a7094a52cdfba2d6f7ce19a
VictorNail <victor.laine@free.fr>
2024-07-01 14:11:25 +0200

    Init

---
+++ src/components/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="item">
+    <h1>Senska</h1>
+  </div>
+</template>
+
+<style scoped>
+    .item {
+        margin-top: 2rem;
+        display: flex;
+        position: relative;
+    }
+</style>
